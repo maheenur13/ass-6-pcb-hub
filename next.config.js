eb13d8782479aa44b7bff086654fa5fb48c62275
jahidun13 <jahidun.nur@apsissolutions.com>
2023-07-29 05:30:05 +0600

    project setup done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["os.alipayobjects.com", "gw.alipayobjects.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import AntdRegistry from "@/libs/AntdRegistry";
+import store from "@/redux/store";
+import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    <Provider store={store}>
+      <AntdRegistry>
+        <ConfigProvider>
+          <SessionProvider session={pageProps.session}>
+            {getLayout(<Component {...pageProps} />)}
+          </SessionProvider>
+        </ConfigProvider>
+      </AntdRegistry>
+    </Provider>
+  );
 }
