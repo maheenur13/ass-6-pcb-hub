cd7349c29c183a502056bd3c58a8579f9a01e74f
jahidun13 <jahidun.nur@apsissolutions.com>
2023-07-30 17:57:02 +0600

    pc builder added to redux

---
+++ src/redux/features/pc-builder/pcbuilder.slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  components: [],
+};
+
+const pcBuilderSlice = createSlice({
+  name: "pcBuilder",
+  initialState,
+  reducers: {
+    setComponent: (state, action) => {
+      state.components.push(action.payload);
+      // state.component = action.payload;
+    },
+    emptyComponent: (state, action) => {
+      state.components = [];
+      // state.component = action.payload;
+    },
+    removeComponent: (state, action) => {
+      state.components = state.components.filter(
+        (itm) => itm.category._id !== action.payload.category._id
+      );
+    },
+  },
+});
+
+export const { removeComponent, setComponent, emptyComponent } =
+  pcBuilderSlice.actions;
+
+export default pcBuilderSlice.reducer;
