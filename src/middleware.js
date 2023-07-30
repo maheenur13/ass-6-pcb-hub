d227b8973c698a195974b7667a1f39a7e9431fda
jahidun13 <jahidun.nur@apsissolutions.com>
2023-07-30 19:15:46 +0600

    middleware fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["https://ass-6-pcb-ktncbgd1i-maheenur13.vercel.app/pc-builder"],
+  matcher: ["/pc-builder"],
 };
