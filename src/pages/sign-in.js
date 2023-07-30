c6e555cf400afda93d92ecbc9ceec8f2020f44fd
jahidun13 <jahidun.nur@apsissolutions.com>
2023-07-30 19:19:53 +0600

    sign in url fix

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -11,7 +11,7 @@ const SignIn = () => {
     // Implement GitHub authentication logic here
     // Redirect the user to the GitHub authentication page
     signIn("github", {
-      callbackUrl: callbackUrl || process.env.PUBLIC_URL,
+      callbackUrl: process.env.PUBLIC_URL,
     });
   };
   const handleGoogleAuth = () => {
